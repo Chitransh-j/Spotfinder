a640ca3e663fd5c936638553343e0dbad6ec6031
Chitransh-j <chitranshjawere3@gmail.com>
2024-07-09 19:51:07 +0530

    added validation and transformations

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,8 +1,17 @@
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
 
 
-export class CreateReservationDto {    
+export class CreateReservationDto {   
+    @IsDate() @Type(()=>Date)
     startDate :Date;
+
+    @IsDate() @Type(()=>Date)
     endDate: Date;
+
+    @IsString() @IsNotEmpty()
     placeId : string;
+
+    @IsString() @IsNotEmpty()
     invoiceId : string;
 }
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,9 +4,14 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationRepository } from './reservations.repository';
 import { ReservationDocument, reservationschema } from './models/reservation.schema';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports:[DatabaseModule,DatabaseModule.forFeature([{name: ReservationDocument.name,schema:reservationschema}])],
+  imports:[DatabaseModule,
+    DatabaseModule.forFeature([{name: ReservationDocument.name,schema:reservationschema}]),
+    LoggerModule
+    
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService,ReservationRepository],
 })
