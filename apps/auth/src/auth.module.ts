48b6a3e8a2da9a7624a07247b980c2c018fbd97d
Chitransh-j <chitranshjawere3@gmail.com>
2024-07-10 17:06:54 +0530

    fixed windows docker issues

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,9 +3,30 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi'
 
 @Module({
-  imports: [UsersModule,LoggerModule],
+  imports: [UsersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      validationSchema: Joi.object({
+        MONGODB_URI : Joi.string().required(),
+        JWT_SECRET : Joi.string().required(),
+        JWT_EXPIRATION : Joi.string().required() 
+      })
+      }),
+    JwtModule.registerAsync({
+      useFactory :(configService: ConfigService) => ({
+        secret:configService.get<string>('JWT_SECRET'),
+        signOptions:{
+          expiresIn: `${configService.get<string>('JWT_EXPIRATION')}s`
+        }
+      }),
+      inject:[ConfigService]
+    })],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -5,12 +5,19 @@ import { DatabaseModule } from '@app/common';
 import { ReservationRepository } from './reservations.repository';
 import { ReservationDocument, reservationschema } from './models/reservation.schema';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi'
 
 @Module({
   imports:[DatabaseModule,
     DatabaseModule.forFeature([{name: ReservationDocument.name,schema:reservationschema}]),
-    LoggerModule
-    
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      validationSchema: Joi.object({
+        MONGODB_URI : Joi.string().required()
+      })
+    })
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService,ReservationRepository],
